2d8b85d3b70b4ab14457658510d518b2203d810d
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-09-23 21:26:57 +0400

    update about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,15 @@
 function About() {
     return (
         <div className="section">
-            About section
+            <div className="about">
+                <h1>
+                    This is a website about space
+                </h1>
+                <p>
+                    It's just a hobby site for myself :) 
+                    Me, the space noob
+                </p>
+            </div>
         </div>
     )
 }
