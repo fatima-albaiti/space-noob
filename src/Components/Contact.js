3e5f524e38eca52045d349c42b3ab2915bb32ca3
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-09-30 14:48:03 +0400

    update contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,18 +3,16 @@ import Form from 'react-bootstrap/Form';
 function Contact() {
     return (
         <div className='section'>
-            <h1>Contact us</h1>
-            <p>Send us an e-mail</p>
+            <h1>Send an e-mail</h1>
+            <p>Got any suggestions or enquiries? Let me know!</p>
             <Form className='contact-form'>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" />
+                <Form.Control placeholder='Subject' className='input-primary' type="text" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control placeholder='Type your message here...' className='input-primary' as="textarea" rows={3} />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button className='button-primary' variant="primary" type="submit">
             Send E-mail
             </Button>
             </Form>
