50922785a230e8bd8037cb439524adc2086ff235
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-09-30 08:11:26 +0400

    update navbar style

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,32 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect } from 'react';
+import '../nav.css';
 
 function NavBar() {
+  //navbar scroll when active state
+  const [navbar, setNavbar] = useState(false)
+  const NAVBAR_HEIGHT = 60;
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    console.log(navbar);
+    if (window.scrollY >= NAVBAR_HEIGHT) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+
+  useEffect(() => {
+    changeBackground()
+    // adding the event when scroll change background
+    window.addEventListener("scroll", changeBackground)
+  })
+
   return (
-    <Navbar data-bs-theme="dark" fixed='top' expand="lg" className="bg-body-tertiary">
+    <Navbar data-bs-theme="dark" fixed='top' expand="lg" className={navbar ? "bg-body-tertiary navbar-custom active" : "bg-body-tertiary navbar-custom"}>
       <Container>
         <Navbar.Brand href="/">Space Noob</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
