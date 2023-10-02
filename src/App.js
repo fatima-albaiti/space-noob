07b0302559803069b9ccabf1fa52793fda62554a
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-10-02 18:55:06 +0400

    minor change

--- src/App.js
+++ src/App.js
@@ -18,11 +18,7 @@ function App() {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'></link>
     <link href="https://fonts.googleapis.com/css2?family=Jura&family=Michroma&display=swap" rel="stylesheet"></link>
     <div className="App">
-
-      
-      
       <header className="App-header">
-        
         <ScrollToTop />
         <NavBar />
         <Routes>
