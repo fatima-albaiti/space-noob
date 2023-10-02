2885dbec587728959108a0a2fcb523f69fa4cb01
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-10-02 14:10:59 +0400

    order posts by date descending

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,92 +2,26 @@ import { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 import { PostType } from "../Objects/PostType";
 import { db } from '../firebase_setup/firebase';
-import {getDocs, collection} from 'firebase/firestore'
+import { collection, query, orderBy, onSnapshot} from 'firebase/firestore'
 function Posts(props) {
     const [postsList, setPostsList] = useState([]);
     const { postType } = props;
 
     useEffect(()=> {
-        //postType === PostType.ARTICLE ?
-        // setPostsList([
-        //     {
-        //         id: 1,
-        //         title: "Article 1 Title",
-        //         content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://m.media-amazon.com/images/I/A1KS+MYYXkL._SL1500_.jpg"
-        //     },
-
-        //     {
-        //         id: 2,
-        //         title: "Article 2 Title",
-        //         content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg"
-        //     },
-
-        //     {
-        //         id: 3,
-        //         title: "Article 3 Title",
-        //         content: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg"
-        //     }
-        // ]) :
-        // setPostsList([
-        //     {
-        //         id: 1,
-        //         title: "Blog 1 Title",
-        //         content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg"
-        //     },
-
-        //     {
-        //         id: 2,
-        //         title: "Blog 2 Title",
-        //         content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg"
-        //     },
-
-        //     {
-        //         id: 3,
-        //         title: "Blog 3 Title",
-        //         content: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?`,
-        //         date: "01/01/2023",
-        //         author: "",
-        //         src: "",
-        //         srcUrl: "",
-        //         imgUrl: "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg"
-        //     }
-        // ])
         fetchData();
     }, [postType]);
 
     const fetchData = async () => {
-        await getDocs(collection(db, "posts"))
-            .then((querySnapshot)=>{               
-                const newData = querySnapshot.docs
-                    .map((doc) => ({...doc.data(), id:doc.id }));
-                console.log(newData);                
-                setPostsList(newData);
-            })
+        const posts_collection = collection(db, "posts")
+        const q = query(posts_collection, orderBy("postDate", "desc"));
+        onSnapshot(q, (querySnapShot) => {
+        const data = querySnapShot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }))
+        setPostsList(data);
+        console.log(data);
+        });
     }
   
     const renderPosts = (list) => {
