98c29b436829a737c3b37014418f7791b45c19ed
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-10-02 14:30:24 +0400

    filter posts by postType + refactor code

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,28 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import PostCard from "./PostCard";
 import { PostType } from "../Objects/PostType";
 import { db } from '../firebase_setup/firebase';
-import { collection, query, orderBy, onSnapshot} from 'firebase/firestore'
+import { collection, query, orderBy, onSnapshot, where } from 'firebase/firestore'
 function Posts(props) {
     const [postsList, setPostsList] = useState([]);
     const { postType } = props;
 
-    useEffect(()=> {
-        fetchData();
-    }, [postType]);
-
-    const fetchData = async () => {
+    const fetchData = useCallback( async () => {
         const posts_collection = collection(db, "posts")
-        const q = query(posts_collection, orderBy("postDate", "desc"));
+        const q = query(posts_collection, orderBy("postDate", "desc"), where("postType", "==", postType));
         onSnapshot(q, (querySnapShot) => {
         const data = querySnapShot.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
         }))
         setPostsList(data);
-        console.log(data);
         });
-    }
+    }, [postType])
+
+    useEffect(()=> {
+        fetchData();
+    }, [fetchData]);
   
     const renderPosts = (list) => {
         return list.map((item)=> (
