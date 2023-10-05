61999efe18185849e422c92a0cf52e950bba2b6f
fatima-albaiti <fatima.albaiti314@gmail.com>
2023-10-06 01:18:51 +0400

    update product card design

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -10,7 +10,7 @@ function Product(props) {
         <Card className="amazon-card">
             <Card.Img src={product.imgUrl}></Card.Img>
             <Card.Body>
-            <Card.Title className="multiline-ellipsis two-lines">{product.productName}</Card.Title>
+            <Card.Title className="multiline-ellipsis one-line">{product.productName}</Card.Title>
                 <Card.Text className="multiline-ellipsis three-lines">{product.productDescription}</Card.Text>
                 <Button className="button-primary" onClick={()=>{handleGoToAmazon(product.productUrl)}}>Go to Amazon</Button>
             </Card.Body>
